52d5a28a77ff8064da3f0a6789f9a635c1342c9a
mathieso <kieran@dolfinity.com>
2014-03-09 11:25:56 -0400

    Inserting patterns

--- sites/all/modules/cybercourse/cybercourse_pattern/cybercourse-pattern-insert.tpl.php
+++ sites/all/modules/cybercourse/cybercourse_pattern/cybercourse-pattern-insert.tpl.php
@@ -4,21 +4,25 @@ Template for displaying a pattern inside a node
  */
 ?>
 <fieldset class="cyco-pattern-insert well well-small">
-  <legend><?php print t('Pattern'); ?></legend>
+  <legend class="cyco-pattern-insert-legend"><span><?php print t('Pattern'); ?></span></legend>
   <p class="cyco-pattern-insert-title">
     <?php print $title; ?>
   </p>
   <?php if ( $situation ) { ?>
-    <p class="cyco-pattern-insert-situation">
-      <span class="situation-label"><?php print t('Situation: '); ?></span>
-      <?php print $situation; ?>
-    </p>
+    <div class="cyco-pattern-insert-situation">
+      <div class="situation-label"><?php print t('Situation: '); ?></div>
+      <div class="situation-description">
+        <?php print $situation; ?>
+      </div>
+    </div>
   <?php } ?>
   <?php if ( $actions ) { ?>
-    <p class="cyco-pattern-insert-actions">
-      <span class="actions-label"><?php print t('Actions: '); ?></span>
-      <?php print $actions; ?>
-    </p>
+    <div class="cyco-pattern-insert-actions">
+      <div class="actions-label"><?php print t('Actions: '); ?></div>
+      <div class="actions-description">
+        <?php print $actions; ?>
+      </div>
+    </div>
   <?php } ?>
   <?php if ( $category_links_rendered ) { ?>
     <p class="cyco-pattern-insert-categories-container">
